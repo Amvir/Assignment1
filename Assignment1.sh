3e474095824495afb45d637dada9aaa396316b5e
Amrita Sanker <amritasanker@microsoft.com>
2023-01-09 15:14:10 +0100

    For loop

--- Assignment1.sh
+++ Assignment1.sh
@@ -30,9 +30,11 @@ export id3=$(uuidgen)_test_vnet
 export id4=$(uuidgen)_test_vnet
 echo $id1 $id2 $id3 $id4
 
+#ARRAY
+
 echo "#Generating 4 resource groups and appending the string test_vnet to the UUID"
  #One variable for location and do it in a loop
-az group create --name $id1 --location westus
+az group create --name $id1, $id2, $id3, $id4 --location westus
 az group create --name $id2 --location westus
 az group create --name $id3 --location westus
 az group create --name $id4 --location westus
@@ -42,7 +44,7 @@ echo "#Creating a virtual network in the resource groups"
 #do it in a loop
 az network vnet create \
   --name myVNet1 \
-  --resource-group $id1 \
+  --resource-group $id \
   --subnet-name default
 
 az network vnet create \
@@ -60,15 +62,33 @@ az network vnet create \
   --resource-group $id4 \
   --subnet-name default 
 
+for i in `seq 1 4`; do 
+az network vnet create \
+  --name myVNet1 \
+  --resource-group $id$i \
+  --subnet-name default
+done
+
+#example of for while loop
+i=0
+while [ $i -ne 4 ]
+do
+        i=$(($i+1))
+        echo $id$i
+done
+
 echo "#List all resource groups in your subscription"
 az group list
 
 
-#Getting the names of all resource groups
+#Getting the names of all resourceups
 az group list | jq 'map(.name)'
 #Selecting the resource group name from each resource
 az resource list | jq 'map(.resourceGroup) | group_by(.) | map({ name: .[0], length: length }) | sort_by(.length) | reverse'
 #Start by pushing all resource groups with items into a bash variable
 RG_NAMES=$(az resource list | jq -r 'map(.resourceGroup) | group_by(.) | map(.[0])')
 #Next, we'll use $RG_NAMES as a substitution into a query against az group list
-az group list | jq -r "map(.name) | map(select(. as \$NAME | $RG_NAMES | any(. == \$NAME) | not)) | sort"
+az group list | jq -r "map(.name) | map(select(. as \$NAME | $RG_NAMES | any(. == \$NAME) | not)) | sort" gro
+
+#Grab the first resource from the output
+az group list --query "[0].name"
